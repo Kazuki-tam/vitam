eb6ca61d42603ed872d647f41486e59d4c3bae17
yonemoto <yonemoto@tam-tam.co.jp>
2021-12-06 01:16:15 +0900

    Update: register serviceWorker

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,7 +3,7 @@ import '/scss/site.scss';
 
 // Import Libraries
 import './libs/smoothScroll';
-import './libs/modal'
+import './libs/modal';
 
 // Import Svelte App
 import App from './App.svelte';
@@ -12,7 +12,18 @@ import App from './App.svelte';
 const targetElement = document.getElementById('app');
 const app = new App({
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  target: targetElement!
+  target: targetElement!,
 });
 
 export default app;
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then((reg) => {
+      console.log(reg);
+    })
+    .catch((error) => {
+      console.log('Registration failed with', error);
+    });
+}
