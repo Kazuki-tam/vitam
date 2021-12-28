165dcf2c59c01aa4a3f88ade4a7bb5d7bea4cebe
Kazuki-tam <36143987+Kazuki-tam@users.noreply.github.com>
2021-12-29 01:53:40 +0900

    Update: tabindex (#24)

--- src/ts/libs/tab.ts
+++ src/ts/libs/tab.ts
@@ -11,6 +11,7 @@
 
 function tab(wrapperId: string, panelName?: string): void {
   const element = document.getElementById(wrapperId);
+  const tabList = element?.querySelector('[role="tablist"]');
   const tabButtonList = element?.querySelectorAll('[role="tab"]');
 
   // Toggle function
@@ -30,15 +31,44 @@ function tab(wrapperId: string, panelName?: string): void {
     event.preventDefault();
   };
 
-  // Tab EventListener
+  // Tab click EventListener
   tabButtonList?.forEach((item) => {
     item.addEventListener('click', toggleTab);
   });
 
-  // Open a specific panel
+  // Tab keydown EventListener
+  let tabFocus = 0;
+  tabList?.addEventListener('keydown', (event: KeyboardEventInit) => {
+    // Detect arrow direction
+    if (event.code === 'ArrowRight' || event.code === 'ArrowLeft') {
+      // Reset tabindex
+      tabButtonList && tabButtonList[tabFocus].setAttribute('tabindex', '-1');
+      // Move Right
+      if (tabButtonList && event.code === 'ArrowRight') {
+        tabFocus += 1;
+        // If you are at the end, go back to the start
+        if (tabFocus >= tabButtonList.length) {
+          tabFocus = 0;
+        }
+      } else if (tabButtonList && event.code === 'ArrowLeft') {
+        tabFocus -= 1;
+        // If you are at the start, move to the end
+        if (tabFocus < 0) {
+          tabFocus = tabButtonList.length - 1;
+        }
+      }
+      // Change tabindex
+      const tabFocused = tabButtonList && (tabButtonList[tabFocus] as HTMLButtonElement);
+      tabFocused && tabFocused.setAttribute('tabindex', '0');
+      tabFocused && tabFocused.focus();
+    }
+  });
+
+  // Open the specific panel
   if (panelName) {
-    const indexTab = element?.querySelector(`[aria-controls="${panelName}"]`) as HTMLElement;
+    const indexTab = element?.querySelector(`[aria-controls="${panelName}"]`) as HTMLButtonElement;
     indexTab?.click();
+    indexTab?.focus();
   }
 }
 
