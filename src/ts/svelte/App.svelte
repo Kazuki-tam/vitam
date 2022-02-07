08bb4e794e1ae7d71335418d0b3a6c5530860406
Kazuki-tam <36143987+Kazuki-tam@users.noreply.github.com>
2022-02-07 11:04:10 +0900

    Remove: svelte env (#39)

--- src/ts/svelte/App.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  import Heading from './components/heading.svelte';
-</script>
-
-<Heading />
-<p>VITAM is a front-end template with Vite for static websites.</p>
--- src/ts/svelte/components/heading.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  const title = 'Welcome to VITAM Docs!';
-</script>
-
-<h1>{title}</h1>
