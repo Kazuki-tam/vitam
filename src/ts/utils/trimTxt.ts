425b96fa66a7e06627596be98bfababbae2e4181
Kazuki-tam <36143987+Kazuki-tam@users.noreply.github.com>
2022-02-09 01:32:01 +0900

    Add: Jest (#40)
    * Add: Jest
    
    * Update: test

--- src/ts/utils/trimTxt.ts
+++ src/ts/utils/trimTxt.ts
@@ -2,12 +2,11 @@
  * Trim Text Function
  * @param { String } text Target text
  * @param { Number } trimLength Word count
- * @param { String } afterText Character string given after trimming
+ * @param { string } afterText Character string given after trimming
+ * @returns { string } Trimed text
  */
 
-function trimTxt(text: string, trimLength: number, afterText: string): string {
-  trimLength = trimLength || 50;
-  afterText = afterText || '…';
+function trimTxt(text: string, trimLength = 50, afterText = '…'): string {
   if (text.length < trimLength) {
     return text;
   }
